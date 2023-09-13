d9a9f682f9199285b9c60aeeacd8e4bc31bacc36
PhillipOdd <135106680+PhillipOdd@users.noreply.github.com>
2023-09-13 12:06:49 +0300

    Add files via upload

---
+++ lib/conference_badges.py
@@ -0,0 +1,26 @@
+def badge_maker(name):
+     return f"Hello, my name is {name}."
+
+def batch_badge_creator(names):
+    badges = []
+    for name in names:
+        badges.append(badge_maker(name))
+    return badges
+
+
+def assign_rooms(names):
+    room_assignments = []
+    for index, name in enumerate(names, start=1):
+        room_assignments.append(f"Hello, {name}! You'll be assigned to room {index}!")
+    return room_assignments
+
+def printer(names):
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+    
+    for badge in badges:
+        print(badge)
+    
+    for assignment in room_assignments:
+        print(assignment)
+
